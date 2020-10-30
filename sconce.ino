37a40dc971d83f59e67328fab020fe390519d626
Aaron Probus <aprobus15@gmail.com>
2020-10-29 21:01:02 -0700

    Sets all LEDs

--- sconce.ino
+++ sconce.ino
@@ -6,26 +6,40 @@
 #include "neopixel.h"
 #include "neopixel_wrapper.h"
 
-const int num_pixels = 60;
-const int neopixel_pin = 6;
+const int num_pixels = 40;
+const int neopixel_pin = 7;
+const int button_pin = A4;
 
 void setup() {
   // Section - Allocate
   Adafruit_NeoPixel raw_pixels{num_pixels, neopixel_pin, NEO_GRBW};
   NeopixelWrapper pixels(&raw_pixels);
+  pinMode(button_pin, INPUT);
 
   // Section - Initialize
   Serial.begin(9600);
   raw_pixels.begin();
   raw_pixels.clear();
 
-  color_t pixel_color = {0, 0, 255, 0};
-  pixels.setPixelColor(0, pixel_color);
-  pixels.setPixelColor(20, pixel_color);
+  color_t pixel_color = {0, 0, 0, 255};
+  /*pixels.setPixelColor(0, pixel_color);*/
+  /*for (int i = 30; i < 47; i++) {*/
+    /*pixels.setPixelColor(i, pixel_color);*/
+  /*}*/
+
+  for (int i = 0; i < num_pixels; i += 1) {
+    pixels.setPixelColor(i, pixel_color);
+  }
+  pixels.show();
 
   // Section - Loop
   while(true) {
-    pixels.show();
+    delay(500);
+    if (digitalRead(button_pin) == HIGH) {
+      Serial.write("high\n");
+    } else {
+      Serial.write("low\n");
+    }
   }
 }
 
